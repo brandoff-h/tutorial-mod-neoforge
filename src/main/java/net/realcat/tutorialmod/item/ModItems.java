824a31369b2add139eab9194141f2b447fdb47c5
brandoff-h <91394528+brandoff-h@users.noreply.github.com>
2025-01-01 23:18:16 -0800

    Add chisel custom item

--- src/main/java/net/realcat/tutorialmod/item/ModItems.java
+++ src/main/java/net/realcat/tutorialmod/item/ModItems.java
@@ -5,6 +5,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.realcat.tutorialmod.TutorialMod;
+import net.realcat.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
@@ -14,6 +15,9 @@ public class ModItems {
 
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
             () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
