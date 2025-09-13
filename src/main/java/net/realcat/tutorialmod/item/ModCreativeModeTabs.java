b7e9fc0677c4932db9e1614e310f955a5ca83166
brandoff-h <91394528+brandoff-h@users.noreply.github.com>
2025-09-12 20:44:30 -0700

    Add radish item and extra textures

--- src/main/java/net/realcat/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/realcat/tutorialmod/item/ModCreativeModeTabs.java
@@ -24,6 +24,7 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.RAW_BISMUTH);
 
                         output.accept(ModItems.CHISEL);
+                        output.accept(ModItems.RADISH);
                     }).build());
 
     public static final Supplier<CreativeModeTab> BISMUTH_BLOCK_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab",
--- src/main/java/net/realcat/tutorialmod/item/ModItems.java
+++ src/main/java/net/realcat/tutorialmod/item/ModItems.java
@@ -19,6 +19,10 @@ public class ModItems {
     public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
             () -> new ChiselItem(new Item.Properties().durability(32)));
 
+    public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
