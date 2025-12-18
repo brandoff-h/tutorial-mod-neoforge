85d6cdd4e2779e5084933cd5f43dc29d44ae8a59
brandoff-h <91394528+brandoff-h@users.noreply.github.com>
2025-12-17 21:19:54 -0800

    Add more datagen classes and generated data

--- src/main/java/net/realcat/tutorialmod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/realcat/tutorialmod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,28 @@
 package net.realcat.tutorialmod.datagen;
 
-public class ModBlockStateProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.realcat.tutorialmod.TutorialMod;
+import net.realcat.tutorialmod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TutorialMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BISMUTH_BLOCK);
+
+        blockWithItem(ModBlocks.BISMUTH_ORE);
+        blockWithItem(ModBlocks.BISMUTH_DEEPSLATE_ORE);
+
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+    }
+
+    private void blockWithItem(DeferredBlock<?> deferredBlock) {
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
 }
--- src/main/java/net/realcat/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/realcat/tutorialmod/datagen/ModItemModelProvider.java
@@ -1,4 +1,25 @@
 package net.realcat.tutorialmod.datagen;
 
-public class ModItemModelProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.realcat.tutorialmod.TutorialMod;
+import net.realcat.tutorialmod.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BISMUTH.get());
+        basicItem(ModItems.RAW_BISMUTH.get());
+
+        basicItem(ModItems.RADISH.get());
+        basicItem(ModItems.STARLIGHT_ASHES.get());
+        basicItem(ModItems.FROSTFIRE_ICE.get());
+
+        basicItem(ModItems.CHISEL.get());
+    }
 }
