732f5dda482b56022bbdeaba3f8706c63ff80e48
brandoff-h <91394528+brandoff-h@users.noreply.github.com>
2025-12-28 08:01:36 -0800

    Add more generators and things like that

--- src/main/java/net/realcat/tutorialmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/realcat/tutorialmod/datagen/ModItemTagProvider.java
@@ -1,4 +1,32 @@
 package net.realcat.tutorialmod.datagen;
 
-public class ModItemTagProvider {
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.realcat.tutorialmod.TutorialMod;
+import net.realcat.tutorialmod.item.ModItems;
+import net.realcat.tutorialmod.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, blockTags, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.BISMUTH.get())
+                .add(ModItems.RAW_BISMUTH.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+    }
 }
